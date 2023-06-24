b972da0cd898a145d5b52e254f3c15f998700488
Rohanth <rohanth3panasa@gmail.com>
2023-06-24 13:48:01 +0530

    Sample Commit

---
+++ Broad/client/src/components/FindUsers.js
@@ -0,0 +1,40 @@
+import {
+  Avatar,
+  Card,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { AiOutlineUser } from "react-icons/ai";
+import { MdRefresh } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { getRandomUsers } from "../api/users";
+import Loading from "./Loading";
+import UserAvatar from "./UserAvatar";
+import HorizontalStack from "./util/HorizontalStack";
+
+const FindUsers = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState(null);
+
+  const fetchUsers = async () => {
+    setLoading(true);
+    const data = await getRandomUsers({ size: 5 });
+    setLoading(false);
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const handleClick = () => {
+    fetchUsers();
+  };
+
+  
+};
+
+export default FindUsers;
